7eaf397799d4277dc7f049b89228ce35ab527d33
Tom <tomg555@gmail.com>
2015-06-15 12:46:49 -0500

    Added models and base methods

---
+++ Models/Candidate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlternativeVote.Models
+{
+    public class Candidate
+    {
+        public Candidate()
+        {
+            VoteCount = 0;
+            IsActive = true;
+        }
+        public int ID { get; set; }
+        public int VoteCount { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using AlternativeVote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlternativeVote
+{
+    class Program
+    {
+        static List<Citizen> citizens;
+        static List<Candidate> candidates;
+        static void Main(string[] args)
+        {
+            InitlializeCandidates();
+            CastVotes();
+            while(!IsElectionDone())
+            {
+                RunElection();
+            }
+            Console.WriteLine("Election Done");
+            Console.WriteLine("Winner: {0}", candidates.Where(c => c.IsActive == true).FirstOrDefault().Name);
+            Console.ReadKey();
+        }
+        static void InitlializeCandidates()
+        {
+            candidates = new List<Candidate>();
+        }
+        static void CastVotes()
+        {
+            citizens = new List<Citizen>();
+        }
+        static void RunElection()
+        {
+            foreach(Citizen citizen in citizens)
+            {
+                int vote = GetNextVote(citizen);
+                candidates.Where(candidate => candidate.ID == vote).FirstOrDefault().VoteCount++;
+            }
+        }
+        static bool IsElectionDone()
+        {
+            return candidates.Where(c => c.IsActive == true).ToList().Count == 1;
+        }
+        static int GetNextVote(Citizen c)
+        {
+            for(int i=0;i<c.votes.Count;i++)
+            {
+                if (CandidateIsActive(c.votes[i]))
+                    return c.votes[i];
+            }
+            return -1;
+        }
+        static bool CandidateIsActive(int id)
+        {
+            return candidates.Where(candidate => candidate.ID == id).FirstOrDefault().IsActive;
+        }
+    }
+}
